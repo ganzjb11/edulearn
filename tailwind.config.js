951480c8abe391b1b3f0faf7f357a91d0c313534
ganzjb11 <verenjunise@gmail.com>
2025-12-06 22:52:35 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-cat > tailwind.config.js << 'EOF'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
@@ -7,4 +6,3 @@ module.exports = {
   },
   plugins: [],
 }
-EOF
